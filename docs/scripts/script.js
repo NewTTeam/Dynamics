13b2d5888770a31ae8a5296a3a1cb41b1ee059e8
Артём Щербаков <artemsch1711@gmail.com>
2025-10-19 19:40:01 +0300

    fix: pages hotfix

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,17 +1,17 @@
-const bottomMenu = document.getElementById("bottomMenu");
-const menuLinks = bottomMenu.querySelectorAll("a");
+// const bottomMenu = document.getElementById("bottomMenu");
+// const menuLinks = bottomMenu.querySelectorAll("a");
 
-menuLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    const id = link.getAttribute("href").substring(1);
-    const target = document.getElementById(id);
-    if (target) {
-      const y = target.getBoundingClientRect().top + window.scrollY - 50;
-      window.scrollTo({ top: y, behavior: "smooth" });
-    }
-  });
-});
+// menuLinks.forEach((link) => {
+//   link.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const id = link.getAttribute("href").substring(1);
+//     const target = document.getElementById(id);
+//     if (target) {
+//       const y = target.getBoundingClientRect().top + window.scrollY - 50;
+//       window.scrollTo({ top: y, behavior: "smooth" });
+//     }
+//   });
+// });
 
 document.querySelectorAll('.md-toggle').forEach(btn => {
   btn.addEventListener('click', () => {
