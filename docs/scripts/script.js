ccc003e88b40fb2430e23665a5d7a63d5e5b360c
Артём Щербаков <artemsch1711@gmail.com>
2025-10-19 17:47:05 +0300

    fix: fixed md display

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -13,21 +13,23 @@ menuLinks.forEach((link) => {
   });
 });
 
-async function parseNaming(elem) {
-  try {
-    const res = await fetch('docs/howtoname.md'); // если index.html тоже в docs
-    if (!res.ok) throw new Error(`Ошибка загрузки: ${res.status}`);
-    const text = await res.text();
-    elem.innerHTML = marked.parse(text);
-  } catch (err) {
-    console.error(err);
-    if (elem)
-      elem.innerHTML = `<p style="color:red">Не удалось загрузить файл: ${err.message}</p>`;
-  }
-}
+// async function parseNaming(elem) {
+// 	console.log(1)
+//   try {
+//     const res = await fetch('docs/howtoname.md'); // если index.html тоже в docs
+//     if (!res.ok) throw new Error(`Ошибка загрузки: ${res.status}`);
+//     const text = await res.text();
+//     elem.innerHTML = marked.parse(text);
+// 		console.log(marked.parse(text))
+//   } catch (err) {
+//     console.error(err);
+//     if (elem)
+//       elem.innerHTML = `<p style="color:red">Не удалось загрузить файл: ${err.message}</p>`;
+//   }
+// }
 
-window.addEventListener('DOMContentLoaded', () => {
-  const target = document.getElementById('md-content');
-  if (target) parseNaming(target);
-});
+// window.addEventListener('DOMContentLoaded', () => {
+//   const target = document.getElementById('md-content');
+//   if (target) parseNaming(target);
+// });
 
